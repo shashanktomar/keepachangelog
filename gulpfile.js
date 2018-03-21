322b382db728a1310f2b8136b0c44531fcf01275
Shashank Tomar <shashank004@gmail.com>
2018-03-21 17:31:30 +0530

    Updated babel version

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,17 @@ var babel = require('gulp-babel');
 
 gulp.task('default', ['compile']);
 
-gulp.task('compile', function () {
-    return gulp.src('src/**/*.js')
-        .pipe(babel({optional: ['runtime']}))
-        .pipe(gulp.dest('dist'));
+gulp.task('compile', function() {
+  return gulp
+    .src('src/**/*.js')
+    .pipe(
+      babel({
+        plugins: ['transform-runtime']
+      })
+    )
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
   gulp.watch('src/**/*.js', ['compile']);
 });
